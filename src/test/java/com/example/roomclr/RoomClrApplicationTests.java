81054bc655c9afd1ce60703e582daefab5ab741e
vshumick <vshumick@gmail.com>
2024-04-19 15:28:03 +0300

    room-clr

---
+++ src/test/java/com/example/roomclr/RoomClrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roomclr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomClrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
